ac691ee91b71337da3515c7fb3b1b2c2a114351a
Colin Thomas Kraczkowsky <Home@Colins-MacBook-Pro-2.local>
2019-12-15 12:56:21 -0800

    Bug fixes.

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,12 @@
 from flask import render_template
 from main import app
 
+@app.route('/basic-template')
+def basicTemplate():
+    return render_template('basic-template.html')
+
 @app.route('/')
+@app.route('/setup')
 def firstPage():
     return render_template('part-one.html')
 
